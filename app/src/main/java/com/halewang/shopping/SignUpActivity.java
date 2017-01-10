e3da5de83bc05bcbb01846ef5b7c9d13a5176710
halewang <862193071@qq.com>
2017-01-10 17:55:21 +0800

    sign up

--- app/src/main/java/com/halewang/shopping/SignUpActivity.java
+++ app/src/main/java/com/halewang/shopping/SignUpActivity.java
@@ -1,6 +1,5 @@
 package com.halewang.shopping;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -9,7 +8,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
